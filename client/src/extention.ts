627268a93586da85e8356ab6bf1ea99f9b9443eb
aion0721 <rudolf.aion@gmail.com>
2025-06-07 18:15:54 +0900

    feat: add client

---
+++ client/src/extention.ts
@@ -0,0 +1,43 @@
+import * as path from "path";
+import * as vscode from "vscode";
+import {
+  LanguageClient,
+  TransportKind,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: vscode.ExtensionContext) {
+  const serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js")
+  );
+  const ServerOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: { execArgv: ["--nolazy", "--inspect=6009"] },
+    },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "yaml" }],
+    synchronize: {
+      fileEvents: vscode.workspace.createFileSystemWatcher("**/.yaml"),
+    },
+  };
+
+  client = new LanguageClient(
+    "yamlLspServer",
+    "YAML LSP Server",
+    ServerOptions,
+    clientOptions
+  );
+  client.start();
+}
+
+export function deactive(): Thenable<void> | undefined {
+  return client ? client.stop() : undefined;
+}
